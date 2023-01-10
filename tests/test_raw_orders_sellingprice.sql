6594568e391536f2f71d10a317e5d1d88e03564c
varunexl23 <varun.bajaj@exlservice.com>
2023-01-10 18:59:21 +0000

    added test to check if Selling price is less than 0

---
+++ tests/test_raw_orders_sellingprice.sql
@@ -0,0 +1,11 @@
+with
+order as (
+    delect * from {{ ref('RAW_ORDERS') }}
+
+)
+SELECT 
+    ORDERID,
+    SUM(ORDERSELLINGPRICE) AS TOTAL_SP
+FROM RAW_ORDERS
+GROUP BY ORDERID
+HAVING TOTAL_SP<0
